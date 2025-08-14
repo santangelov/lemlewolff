8bbfe4aba6b2a9e1299b47395d7f148018553e91
Vincent Santangelo <42481209+santangelov@users.noreply.github.com>
2025-08-14 18:05:36 +0300

    Add ExternalApps controller

---
+++ LW_Web/Controllers/ExternalAppsController.cs
@@ -0,0 +1,41 @@
+using LW_Data;
+using LW_Security;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace LW_Web.Controllers
+{
+    public class ExternalAppsController : Controller
+    {
+        private readonly LWDbContext _context;
+
+        public ExternalAppsController()
+        {
+            _context = new LWDbContext();
+        }
+
+        // GET: ExternalApps
+        public ActionResult Index()
+        {
+            if (!clsSecurity.isUserLoggedIn())
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            var apps = _context.tblAdminApps
+                                .OrderBy(a => a.AppName)
+                                .ToList();
+
+            return View(apps);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

95f3eb065728b6f77bfb7f8cbe098be9bb0ebeda
Vincent Santangelo <42481209+santangelov@users.noreply.github.com>
2026-03-02 20:25:23 +0200

    Simplify dashboard to only show arrears loaded-through date

--- LW_Web/ViewModels/DashboardModel.cs
+++ LW_Web/ViewModels/DashboardModel.cs
@@ -1,8 +1,6 @@
-﻿using LW_Common;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Web;
-using System.Web.Mvc;
+using LW_Common;
+using LW_Data;
+using System;
 
 namespace LW_Web.ViewModels
 {
@@ -10,8 +8,6 @@ namespace LW_Web.ViewModels
     {
         public DashboardModel()
         {
-            // Look up the Import Dates
-            clsReportHelper RH = new clsReportHelper();
             SortlyDateRangeLoaded = clsReportHelper.GetFileDateRangeValues("Sortly").DateRangeAsString;
             YardiWODateRangeLoaded = clsReportHelper.GetFileDateRangeValues("YardiWO_File").DateRangeAsString;
             YardiPODateRangeLoaded = clsReportHelper.GetFileDateRangeValues("YardiPO_File").DateRangeAsString;
@@ -20,33 +16,43 @@ namespace LW_Web.ViewModels
             YardiWOGeneralDateRangeLoaded = clsReportHelper.GetFileDateRangeValues("YardiWO_GeneralFile").DateRangeAsString;
             YardiPropertyAndUnitDateRangeLoaded = clsReportHelper.GetFileDateRangeValues("YardiPropertyFile").DateRangeAsString;
             ADPDateRangeLoaded = clsReportHelper.GetFileDateRangeValues("ADP").DateRangeAsString;
+
+            DashboardAsOfTimestamp = DateTime.Now.ToString("M/d/yy h:mm tt");
+            SetArrearsDataCoverageDates();
         }
 
-        [DisplayName("Sortly Date Range Loaded")]
+        private void SetArrearsDataCoverageDates()
+        {
+            ArrearsDataLoadedThrough = "No snapshot found";
+
+            clsDataHelper H = new clsDataHelper();
+            H.cmd.Parameters.AddWithValue("@RequestedAsOfDate", DateTime.Today);
+            var row = H.GetDataRow("spQA_ArrearsTracker_DateResolution");
+
+            if (row == null)
+            {
+                return;
+            }
+
+            if (DateTime.TryParse(row["LatestDaily"]?.ToString(), out DateTime latestDailyDate))
+            {
+                ArrearsDataLoadedThrough = latestDailyDate.ToString("M/d/yy");
+            }
+
+        }
+
         public string SortlyDateRangeLoaded { get; set; }
-
-        [DisplayName("Yardi WO Date Range Loaded")]
         public string YardiWODateRangeLoaded { get; set; }
-
-        [DisplayName("Yardi WO Date Range Loaded")]
         public string YardiPODateRangeLoaded { get; set; }
-
-        [DisplayName("Yardi WO Date Range Loaded")]
         public string YardiWO2DateRangeLoaded { get; set; }
-
-        [DisplayName("Yardi WO Date Range Loaded")]
         public string YardiPO2DateRangeLoaded { get; set; }
-
-        [DisplayName("Yardi WO General Date Range Loaded")]
         public string YardiWOGeneralDateRangeLoaded { get; set; }
-
-        [DisplayName("Yardi Property and Unit File Loaded")]
         public string YardiPropertyAndUnitDateRangeLoaded { get; set; }
-
-        [DisplayName("ADP Date Range Loaded")]
         public string ADPDateRangeLoaded { get; set; }
 
-        public string ErrorMsg { get; set; }    
+        public string DashboardAsOfTimestamp { get; set; }
+        public string ArrearsDataLoadedThrough { get; set; }
 
+        public string ErrorMsg { get; set; }
     }
 }
